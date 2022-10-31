2958227a865fa18d2fe58a3a2cb3a0d422775490
cawfeecake <cawfeecake@icloud.com>
2022-10-31 06:38:32 -0700

    begin app.py refactor.. moved movie route to dir; put db create in func
    (probably) made back navigation behavior better on movie form submit

--- app.py
+++ app.py
@@ -1,110 +1,69 @@
-import json
-from markupsafe import escape
-from flask import Flask, abort, redirect, request, url_for
+from flask import Flask, request, url_for # remove request after moving all routes
+
+
+_app = Flask(__name__)
+
+
+def create_backend(app, db_path):
+    from flask_sqlalchemy import SQLAlchemy
+
+    db = SQLAlchemy()
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+    db.init_app(app)
+
+    # toggle this to determine what we need for adding tables, etc.
+    #if not os.path.exists(DB_NAME):
+    #    with app.app_context():
+    #        db.create_all()
+    #    print(f'created tables in backend.')
+
+
+_db_path = 'backend.db'
+create_backend(_app, _db_path)
+
+
+index_funcs = []
+with _app.app_context():
+    import routes.movies
+    index_funcs.append(('movies', 'get_movies'))
+
+    #import routes.locations
+    #index_funcs.append('locations')
+    #index_funcs.append(('locations', 'get_locations'))
+
+    #import routes.items
+    #index_funcs.append('items')
+    #index_funcs.append(('items', 'get_items'))
+
+
+@_app.route('/')
+def index():
+    # TODO add stats about backend.db file
+    return f"""
+        <h1>Python C. R. U. D.</h1>
+        <h2>Pages:</h2>
+        <ul>
+        { ''.join([ f'<li><a href="{ url_for(func) }">{ name }</a></li>' for (name, func) in index_funcs ]) }
+        </ul>
+        <h2>Backend stats:</h2>
+        <p>for file {_db_path}...</p>
+    """
+
+
+# ====================================
 
 
 # Import depended on tables here
-from databases.movies import *
 from databases.items import *
-
-
-app = Flask(__name__)
 
 
 # for DEBUG;;
 from databases.database_tools import *
 # DEBUG;; TOREMOVE
-print(get_all_tables('backend.db'))
+print(get_all_tables(_db_path))
 # TROUBLESHOOTING;;
-#delete_by_id('backend.db', 'movies', 14)
-#delete_table('backend.db', 'items')
-
-
-@app.route('/')
-def index():
-    return f"""
-        <h1>What's in the "movies" table?</h1>
-        <p>There are {len(movies.get_all())} movies in the database!</p>
-    """
-
-
-# data = [
-#     ('Monty Python and the Holy Grail', 1975, p1),
-#     ('And Now for Something Completely Different', 1971, p2),
-#     ('Another movie', 1977, p3),
-# ]
-@app.route('/movies/', methods=['GET', 'POST'])
-def get_movies():
-    # write to movies if a POST request
-    insert_ids = None # declare variables before if block if they are used outside scope
-    if request.method == 'POST':
-        movie_name = request.form.get('name')
-        movie_year = request.form.get('year')
-        rating_x, rating_y = (float(request.form.get('x')), float(request.form.get('y')))
-        insert_ids = movies.insert_data([(movie_name, movie_year, Point(rating_x, rating_y))])
-        # use a return here if you want to redirect away when doing POST
-        #return f"""
-        #    <h1>Added the movie: {movie_name}</h1>
-        #    <pre>{insert_id[0]}</pre>
-        #"""
-    all_movies = movies.get_all()
-    return f"""
-        <form method="POST">
-          <div><label>Name: <input type="text" name="name"></label></div>
-          <div><label>Year: <input type="text" name="year"></label></div>
-          <div><label>Rating Point: <input type="text" name="x"><input type="text" name="y"></label></div>
-          <input type="submit" value="Submit">
-        </form>
-        <hr />
-        { f'<p>Inserted: { " ".join([str(i) for i in insert_ids]) }</p>' if insert_ids else '' }
-        <ol>
-          <li>{ '</li><li>'.join(get_html_movie(m) for m in all_movies) }</li>
-        </ol>
-    """
-
-
-def get_html_movie(movie):
-    movie_id = movie[0]
-    movie_name = movie[1]
-    movie_year = movie[2]
-    return f"""
-        <div>
-          <a href="{ url_for('get_movie_by_id', movie_id=movie_id) }">{movie_name}, ({movie_year})</a>
-        </div>
-    """
-
-
-# TODO if able, use PUT instead of POST as it is more appropriate as it is an update
-@app.route('/movies/<int:movie_id>/', methods=['GET', 'DELETE', 'POST'])
-def get_movie_by_id(movie_id):
-    if request.method == 'DELETE':
-        movies.delete_by_id(movie_id)
-        return 'OK', 204
-    elif request.method == 'POST':
-        movie_name = request.form.get('name')
-        movie_year = request.form.get('year')
-        rating_x, rating_y = (float(request.form.get('x')), float(request.form.get('y')))
-        update_data = [movie_name, movie_year, Point(rating_x, rating_y)]
-        movies.update_by_id(movie_id, update_data)
-        # have this method handler "waterfall" into default (i.e. GET) request path; will get current info for $movie_id
-    return f"""
-        <p>{ movies.get_by_id(movie_id) }</p>
-        <hr />
-        <form method="POST">
-          <div><label>Name: <input type="text" name="name"></label></div>
-          <div><label>Year: <input type="text" name="year"></label></div>
-          <div><label>Rating Point: <input type="text" name="x"><input type="text" name="y"></label></div>
-          <input type="submit" value="Update">
-        </form>
-        <button onclick="delete_movie()">Delete</button>
-        <script>
-          function delete_movie() {{
-            const delete_url = '{ url_for('get_movie_by_id', movie_id=movie_id) }';
-            fetch(delete_url, {{ method: 'DELETE' }})
-              .then(() => {{ window.location.href = '{ url_for('get_movies') }'; }})
-          }}
-        </script>
-    """
+#delete_by_id(_db_path, 'movies', 14)
+#delete_table(_db_path, 'items')
 
 
 # data = [
@@ -112,7 +71,7 @@ def get_movie_by_id(movie_id):
 #     ('comb', 'used to brush my hair', []),
 #     ('cabinet', '3 stacked plastic drawers', [-1]),
 # ]
-@app.route('/items/', methods=['GET', 'POST'])
+@_app.route('/items/', methods=['GET', 'POST'])
 def get_items():
     insert_ids = None
     if request.method == 'POST':
@@ -135,7 +94,7 @@ def get_items():
     """
 
 
-@app.route('/items/<int:item_id>/', methods=['GET', 'DELETE', 'POST'])
+@_app.route('/items/<int:item_id>/', methods=['GET', 'DELETE', 'POST'])
 def get_item_by_id(item_id):
     if request.method == 'DELETE':
         items.delete_by_id(item_id)
@@ -159,22 +118,24 @@ def get_item_by_id(item_id):
 
 
 # TODO: TOREMOVE
-@app.route('/about/')
-def about():
-    return '<h3>This is a Flask web application.</h3>'
-
-@app.route('/capitalize/<word>/')
-def capitalize(word):
-    return '<h1>{}</h1>'.format(escape(word.capitalize()))
-
-@app.route('/add/<int:n1>/<int:n2>/')
-def add(n1, n2):
-    return '<h1>{}</h1>'.format(n1 + n2)
-
-@app.route('/users/<int:user_id>/')
-def greet_user(user_id):
-    users = ['Bob', 'Jane', 'Adam']
-    try:
-        return '<h2>Hi {}</h2>'.format(users[user_id])
-    except IndexError:
-        abort(404)
+#@_app.route('/about/')
+#def about():
+#    return '<h3>This is a Flask web application.</h3>'
+#
+#from markupsafe import escape
+#@_app.route('/capitalize/<word>/')
+#def capitalize(word):
+#    return '<h1>{}</h1>'.format(escape(word.capitalize()))
+#
+#@_app.route('/add/<int:n1>/<int:n2>/')
+#def add(n1, n2):
+#    return '<h1>{}</h1>'.format(n1 + n2)
+#
+#...from flask import abort
+#@_app.route('/users/<int:user_id>/')
+#def greet_user(user_id):
+#    users = ['Bob', 'Jane', 'Adam']
+#    try:
+#        return '<h2>Hi {}</h2>'.format(users[user_id])
+#    except IndexError:
+#        abort(404)
---
+++ routes/movies.py
@@ -0,0 +1,84 @@
+from flask import current_app as app, redirect, request, url_for
+
+
+from databases.movies import *
+
+
+# data = [
+#     ('Monty Python and the Holy Grail', 1975, p1),
+#     ('And Now for Something Completely Different', 1971, p2),
+#     ('Another movie', 1977, p3),
+# ]
+@app.route('/movies/', methods=['GET', 'POST'])
+def get_movies():
+    # write to movies if a POST request
+    insert_ids = None # declare variables before if block if they are used outside scope
+    if request.method == 'POST':
+        movie_name = request.form.get('name')
+        movie_year = request.form.get('year')
+        rating_x, rating_y = (float(request.form.get('x')), float(request.form.get('y')))
+        insert_ids = movies.insert_data([(movie_name, movie_year, Point(rating_x, rating_y))])
+        # use a return here if you want to redirect away when doing POST
+        #return f"""
+        #    <h1>Added the movie: {movie_name}</h1>
+        #    <pre>{insert_id[0]}</pre>
+        #"""
+    all_movies = movies.get_all()
+    return f"""
+        <form method="POST">
+          <div><label>Name: <input type="text" name="name"></label></div>
+          <div><label>Year: <input type="text" name="year"></label></div>
+          <div><label>Rating Point: <input type="text" name="x"><input type="text" name="y"></label></div>
+          <input type="submit" value="Submit">
+        </form>
+        <hr />
+        { f'<p>Inserted: { " ".join([str(i) for i in insert_ids]) }</p>' if insert_ids else '' }
+        <ol>
+          { ''.join([f'<li>{ get_html_movie(m) }</li>' for m in all_movies ]) }
+        </ol>
+    """
+
+
+def get_html_movie(movie):
+    movie_id = movie[0]
+    movie_name = movie[1]
+    movie_year = movie[2]
+    return f"""
+        <div>
+          <a href="{ url_for('get_movie_by_id', movie_id=movie_id) }">{movie_name}, ({movie_year})</a>
+        </div>
+    """
+
+
+# TODO if able, use PUT instead of POST as it is more appropriate as it is an update
+@app.route('/movies/<int:movie_id>/', methods=['GET', 'DELETE', 'POST'])
+def get_movie_by_id(movie_id):
+    if request.method == 'DELETE':
+        movies.delete_by_id(movie_id)
+        return 'OK', 204
+    elif request.method == 'POST':
+        movie_name = request.form.get('name')
+        movie_year = request.form.get('year')
+        rating_x, rating_y = (float(request.form.get('x')), float(request.form.get('y')))
+        update_data = [movie_name, movie_year, Point(rating_x, rating_y)]
+        movies.update_by_id(movie_id, update_data)
+        return redirect(url_for('get_movie_by_id', movie_id=movie_id)) # TODO continue to verify this has desired back-nav. behavior
+        # note: when "waterfalling" from handling POST req., will add to your window history and make back-nav. difficult
+    return f"""
+        <p>{ movies.get_by_id(movie_id) }</p>
+        <hr />
+        <form method="POST">
+          <div><label>Name: <input type="text" name="name"></label></div>
+          <div><label>Year: <input type="text" name="year"></label></div>
+          <div><label>Rating Point: <input type="text" name="x"><input type="text" name="y"></label></div>
+          <input type="submit" value="Update">
+        </form>
+        <button onclick="delete_movie()">Delete</button>
+        <script>
+          function delete_movie() {{
+            const delete_url = '{ url_for('get_movie_by_id', movie_id=movie_id) }';
+            fetch(delete_url, {{ method: 'DELETE' }})
+              .then(() => {{ window.location.href = '{ url_for('get_movies') }'; }})
+          }}
+        </script>
+    """
